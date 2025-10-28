632cbc5d718c8494346ab7c2f66f9e778af62f7a
jaspe <jaspe@ThinkPad_Jasper>
2025-10-28 13:48:42 +0100

    Added main script

---
+++ StrongmanGameMainScript.m
@@ -0,0 +1,11 @@
+% The Strongman Game - main script - v0.1
+% Calls all subscripts to run The Strongman Game
+% Made by UTWENTE-BSC-EE-ESA group 3
+% version 0.1
+disp("Starting The Strongman Game")
+
+disp("Calling Motor Control script")
+StrongmanGameMotorControl() % Calls motor control script
+
+disp("Calling light triggers script")
+StrongmanGameLightTriggers() % Calls light triggers script
