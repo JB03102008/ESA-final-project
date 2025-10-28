f2196874805358787f0188c5a3e5b94243b8a637
jaspe <jaspe@ThinkPad_Jasper>
2025-10-28 14:30:31 +0100

    Added StrongmanGame function

--- StrongmanGame.m
+++ StrongmanGame.m
@@ -1,14 +1,15 @@
+function = StrongmanGame(deviceID, motorChannel, solenoidChannel, LEDpowerChannel, PhotoDiodeinputChannel)
 % The Strongman Game - main script - v0.1
 % Calls all subscripts to run The Strongman Game
 % Made by UTWENTE-BSC-EE-ESA group 3
 % version 0.1
 
 % AD3 IO configuration
-deviceID = "AD3_0";
-motorChannel = "ao0";
-solenoidChannel = "dio00";
-LEDpowerChannel = "V+";
-PhotoDiodeinputChannel = "ai1";
+if nargin < 1, deviceID = "AD3_0";
+if nargin < 2, motorChannel = "ao0";
+if nargin < 3, solenoidChannel = "dio00";
+if nargin < 4, LEDpowerChannel = "V+";
+if nargin < 5, PhotoDiodeinputChannel = "ai1";
 
 disp("Starting The Strongman Game")
 
