e5b957d74bf239bf8d73a20f356aacd2c8aeb750
jaspe <jaspe@ThinkPad_Jasper>
2025-10-29 12:21:54 +0100

    Added rotary display driver

---
+++ StrongmanGameRotaryDisplayDriver.m
@@ -0,0 +1,9 @@
+function StrongmanGameRotaryDisplayDriver(DisplayNumber)
+% The Strongman Game - rotary display driver script version 0.1
+% Displays a number defined in the input variable on a rotary display
+% Made by UTWENTE-BSC-EE-ESA group 3
+% version 0.1
+
+% Contents will be added later
+
+end
